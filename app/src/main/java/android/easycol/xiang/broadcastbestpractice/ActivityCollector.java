79ecc4db0b8ffbddff8cec60ea975f6b40fc15f7
xiang <1806322519@qq.com>
2017-03-07 21:49:51 +0800

    first commit

---
+++ app/src/main/java/android/easycol/xiang/broadcastbestpractice/ActivityCollector.java
@@ -0,0 +1,31 @@
+package android.easycol.xiang.broadcastbestpractice;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xiang on 2017/3/7.
+ */
+public class ActivityCollector {
+
+    public static List<Activity> activities = new ArrayList<>();
+
+    public static void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    public static void removeActivity(Activity activity) {
+        activities.remove(activity);
+    }
+
+    public static void finishAll() {
+        for (Activity activity: activities) {
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+    }
+
+}
